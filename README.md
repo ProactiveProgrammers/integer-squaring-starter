c605dda91551db49e38e361e3fbc7ec58391365e
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-09-09 12:20:04 -0400

    feat: Add all of the template files for integer-squaring-starter.

---
+++ README.md
@@ -0,0 +1,18 @@
+# Integer Squaring
+
+## Assigned: Thursday, September 9, 2021
+## Due: Thursday, September 16, 2021
+
+After cloning this repository to your computer, please take the following steps:
+
+- Follow the instructions on the Proactive Programmers web site for this project
+- Use the `cd` command to change into the directory for this repository.
+- Change into the program directory by typing `cd square`.
+- Install the dependencies for the project by typing `poetry install`
+- Run the program in its two different modes by typing:
+  - `poetry run square --approach for --dir input --file numbers.txt`
+  - `poetry run square --approach while --dir input --file numbers.txt`
+  - Please note that the program will not work unless you add the required source code
+- Confirm that the program is producing the expected output
+- Use a `docker run` command for your operating system to run GatorGrader
+- Provide all of the required responses in the `writing/reflection.md` file
---
+++ square/tests/test_square.py
@@ -0,0 +1,54 @@
+"""Test suite to ensure that each function words correctly."""
+
+from square import __version__
+
+from square import main
+
+
+def test_version():
+    """Confirm that the version of the program is correct."""
+    assert __version__ == "0.1.0"
+
+
+def test_compute_square_while_loop_positive():
+    """Confirm that the while loop calculates squares correctly for positives."""
+    value = 3
+    square_value = main.compute_square_while(value)
+    assert square_value == 9
+
+
+def test_compute_square_while_loop_negative():
+    """Confirm that the while loop calculates squares correctly for negatives."""
+    value = -3
+    square_value = main.compute_square_while(value)
+    assert square_value == 9
+
+
+def test_compute_square_for_loop_positive():
+    """Confirm that the for loop calculates squares correctly for positives."""
+    value = 3
+    square_value = main.compute_square_for(value)
+    assert square_value == 9
+
+
+def test_compute_square_for_loop_negative():
+    """Confirm that the for loop calculates squares correctly for negatives."""
+    value = -3
+    square_value = main.compute_square_for(value)
+    assert square_value == 9
+
+
+def test_compute_square_iterative_for_loop():
+    """Confirm that the for loop calculates squares correctly for negatives and positives in loop."""
+    number_list = """-72
+        29
+        61
+        -42
+        44"""
+    square_function = main.compute_square_for
+    square_list = main.compute_square_iterative(number_list, square_function)
+    assert square_list == [72 * 72, 29 * 29, 61 * 61, 42 * 42, 44 * 44]
+
+
+# TODO: Add a test case for confirming the correctness of the compute_square_iterative function
+# when it is using the square_function as main.compute_square_for
